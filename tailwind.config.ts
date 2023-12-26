14ee8fb4c4c9d6ae42099589529240fc836da390
Gilberto Mossmann <gilbertomossmann@gmail.com>
2023-12-26 16:03:40 -0300

    feat: add commitlint/husky config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
-  theme: {},
-  plugins: []
+  plugins: [],
+  theme: {}
 }
 export default config
