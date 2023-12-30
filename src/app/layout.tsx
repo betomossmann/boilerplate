401cea65aa757ea4921cde40e3d8d7c2d54af75e
Gilberto Mossmann <gilbertomossmann@gmail.com>
2023-12-30 20:40:40 -0300

    chore: add shadcn UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
-import './globals.css'
+import './styles/globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,11 +11,7 @@ export const metadata: Metadata = {
   title: 'Create Next App'
 }
 
-export default function RootLayout({
-  children
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
