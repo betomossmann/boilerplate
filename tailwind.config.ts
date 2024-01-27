8be82e956c49f76d8306812ea49e521421f67135
Gilberto Mossmann <gilbertomossmann@gmail.com>
2024-01-27 13:31:02 -0300

    update config and libs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,9 +7,10 @@ const config = {
   theme: {
     container: {
       center: true,
-      padding: '2rem',
-      screens: {
-        '2xl': '1400px'
+      padding: {
+        DEFAULT: '1rem',
+        lg: '2rem',
+        md: '1.5rem'
       }
     },
     extend: {
